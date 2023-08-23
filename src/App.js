5661ee576834a52cff1ad9de3e34091677ed9c81
cebisli <ikramcebisli@hotmail.com>
2023-08-23 17:08:55 +0300

    State kavramı üzerinde çalışıldı.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Course from './Course';
+
+function getRandomCourse(){
+  const courseArray = ['Angular','Bootstrap','Ccharp','KompleWeb'];
+  return courseArray[Math.floor(Math.random()*courseArray.length)];
+}
 
 function App() {
+
+  const [courses, setCourses] = useState([]);
+
+  const KursEkle = () => {
+      setCourses([...courses, getRandomCourse()])
+  }
+  
+  const KursList = courses.map((course,index) => {
+    return <Course key={index} courseName={course}/>
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <button className='appButton' onClick={KursEkle}>Kurs Ekle</button>
+      <div className='courselist'>{KursList}</div>
+
     </div>
   );
 }
